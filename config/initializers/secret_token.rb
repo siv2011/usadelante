5336439301160f4778d9cc3b6f23b21aa3d08ad7
Sivakami Sambasivam <ssivakami@alum.mit.edu>
2012-06-23 15:14:14 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Usadelante::Application.config.secret_token = 'b389ac89c49c5aa500e543f58a351cdb136fdd3b82113ca7469dd2cd061a354d1798f1afe73ab04ee97a3b7e5b6f2d522c48c4195746241ee70c83f4656f988e'
